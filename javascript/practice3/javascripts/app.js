62890df089e4dc6b7ec83aafcd2bb3f0fd1dbd38
Mondale Felix <mondalefelix@yahoo.com>
2018-02-06 05:46:55 -0800

    practiced if statements

---
+++ javascript/practice3/javascripts/app.js
@@ -0,0 +1,9 @@
+var age = prompt("What is your age?");
+
+if(age < 18){
+  console.log("You cannot enter the bar")
+} else if (age < 21) {
+  console.log("You can enter but you cannot drink")
+} else {
+  console.log("Come in homie")
+}
