6b22bd36094b8a21235d24da881665a26ec80042
Mondale Felix <mondalefelix@yahoo.com>
2018-02-06 05:47:11 -0800

    practice more if statements

--- javascript/practice3/javascripts/app.js
+++ javascript/practice3/javascripts/app.js
@@ -1,9 +1,9 @@
 var age = prompt("What is your age?");
 
-if(age < 18){
-  console.log("You cannot enter the bar")
-} else if (age < 21) {
-  console.log("You can enter but you cannot drink")
-} else {
-  console.log("Come in homie")
+if(age < 1){
+  console.log("Not a valid age");
+} else if(age === 21){
+  console.log("Happy 21st Birthday!");
+} else if(age % 2 === 1){
+  console.log("Your age is odd");
 }
