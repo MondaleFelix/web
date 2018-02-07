ffbba765e414c9dcbdc439a7502aabc10004ae9f
Mondale Felix <mondalefelix@yahoo.com>
2018-02-06 20:38:22 -0800

    practicing javascript fundamentals

---
+++ javascript/practice5/javascripts/app.js
@@ -0,0 +1,61 @@
+
+// 1) Print the values from -10 to 19
+// let initialValue = -10;
+// let endValue = 19;
+
+// while(!(initialValue > endValue)){
+//   console.log(initialValue);
+//   initialValue++
+// }
+
+// 2) Print the even numbers from 10 to 40
+
+// var initialValue = 10;
+// var endValue = 40;
+
+// while(initialValue <= endValue){
+//   if(initialValue % 2 === 0){
+//     console.log(initialValue)
+//   }
+//   initialValue++;
+// }
+
+// 3) Print all odd numbers from 300 to 333
+
+// var initialValue = 300;
+// var endValue = 333;
+
+// while(initialValue <= endValue){
+//   if(initialValue % 2 !== 0){
+//     console.log(initialValue);
+//   }
+//   initialValue++;
+// }
+
+
+// 4) Print all numbers divisable by 5 and 3 from 5 and 50
+
+var initialValue = 5;
+let endValue = 50;
+
+while(initialValue <= endValue){
+  if(initialValue % 5 === 0 && initialValue % 3 === 0){
+    console.log(initialValue);
+  }
+  initialValue++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/practice6/javascripts/app.js
@@ -0,0 +1,7 @@
+var answer = prompt("Are we there yet?")
+
+while(answer !== "yes" && answer !== "yea"){
+  var answer = prompt("Are we there yet?");
+}
+
+alert("yea we made it")
