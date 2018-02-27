e638aa8fff42e0add7a0ba4383bdcdcd5474e6d2
Mondale Felix <mondalefelix@yahoo.com>
2018-02-27 02:40:43 -0800

    fixed event bubbling bug

--- jQuery/TodoListProject/assets/js/todos.js
+++ jQuery/TodoListProject/assets/js/todos.js
@@ -2,3 +2,12 @@
 $("li").on("click", function(){
   $(this).toggleClass("completed");
 })
+
+// Click on X to delete todo
+S("span").on("click", function(event){
+
+
+  // Stops all parent events from firing off
+  event.stopPropagation();
+
+})
