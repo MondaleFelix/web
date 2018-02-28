8f23b8c06e17bb617af6c38fd391556c5f0d448f
Mondale Felix <mondalefelix@yahoo.com>
2018-02-28 00:09:41 -0800

    used a star route

--- node/ExpressApp/app.js
+++ node/ExpressApp/app.js
@@ -11,7 +11,13 @@ app.get("/bye", function(req, res){
 
 app.get("/dog", function(req, res){
   res.send("MEOWW");
-})
+});
+
+
+// REDIRECTS USER THIS WHEN THE TRY TO ACCES A ROUTE NOT DEFINED
+app.get("*", function(req, res){
+  res.send("YOU ARE A STAR")
+});
 
 app.listen(3000, function(){
   console.log("Listening on port 3000 fool");
