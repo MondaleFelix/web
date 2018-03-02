f213de3216cf73829392962bc016a9771e9dcee6
Mondale Felix <mondalefelix@yahoo.com>
2018-03-01 23:58:59 -0800

    made an express app

---
+++ node/ExpressApp/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+  res.send("Hi there!");
+});
+
+app.get("/bye", function(req, res){
+  res.send("Goodbye!");
+});
+
+app.get("/dog", function(req, res){
+  res.send("MEOWW");
+})
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000 fool");
+})
