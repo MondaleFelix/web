81892134feb5c3a593345595ea886604b1e4eee0
Mondale Felix <mondalefelix@yahoo.com>
2018-03-03 19:49:31 -0800

    practised making dynamic web pages

--- node/EJSDemo/app.js
+++ node/EJSDemo/app.js
@@ -10,6 +10,15 @@ app.get("/fallinlovewith/:thing", function(req,res){
   res.render("love.ejs", {thingVar: thing})
 });
 
+app.get("/posts", function(req,res){
+  var posts = [
+    {title: "Post 1", author: "Suzy"},
+    {title: "Post 2", author: "Lazy"},
+    {title: "Post 3", author: "Turdy"}
+  ];
+  res.render("posts.ejs", {posts: posts})
+});
+
 app.listen(3000, function(){
   console.log("Server is listening on port 3000");
 })
