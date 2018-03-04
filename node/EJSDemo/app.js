ef4af1185453b1b6f03fda2889cbfd6174798c2f
Mondale Felix <mondalefelix@yahoo.com>
2018-03-03 19:55:24 -0800

    made a style sheet

--- node/EJSDemo/app.js
+++ node/EJSDemo/app.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 
+
+// Tells express to serve out content from public directory
+app.use(express.static("public"));
+
+
 app.get("/", function(req, res){
   res.render("home.ejs");
 });
