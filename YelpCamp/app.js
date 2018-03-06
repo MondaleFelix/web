32546f89ee07c51e846033d06365acb8c4e77024
Mondale Felix <mondalefelix@yahoo.com>
2018-03-05 22:04:07 -0800

    add function works

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+var campgrounds = [
+  { name : "Salmon Creek", image: "http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
+  { name : "Granite Hill", image: "https://acadiamagic.com/1170px/blackwoods-1197.jpg"},
+  { name : "Mountain Goat's Rest", image: "http://avaloncampground.com/wp-content/uploads/2013/07/Avalon-Campground-table-woods.png"}
+]
 
 // Syntax to use body-parser in our app
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,11 +19,6 @@ app.get("/", function(req,res){
 });
 
 app.get("/campgrounds", function(req,res){
-  var campgrounds = [
-    { name : "Salmon Creek", image: "http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
-    { name : "Granite Hill", image: "https://acadiamagic.com/1170px/blackwoods-1197.jpg"},
-    { name : "Mountain Goat's Rest", image: "http://avaloncampground.com/wp-content/uploads/2013/07/Avalon-Campground-table-woods.png"}
-  ]
 
   res.render("campgrounds", {campgrounds: campgrounds});
 });
@@ -30,7 +30,9 @@ app.post("/campgrounds", function(req,res){
   // Grabs data sent in the "new.ejs" form
   var name = req.body.name;
   var image = req.body.image;
-
+  var newCampgrounds = { name : name , image: image};
+  campgrounds.push(newCampgrounds);
+  res.redirect("/campgrounds")
   // get data from form and add to campgrounds array
 
   // redirect back to campgrounds page
@@ -38,6 +40,7 @@ app.post("/campgrounds", function(req,res){
 
 app.get("/campgrounds/new", function(req,res){
   res.render("new.ejs");
+  campgrounds.push("")
 })
 
 app.listen(3000, function(){
